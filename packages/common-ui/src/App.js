4c5fcd266f0475b427f2a74ec0832eb1012177fa
Vempalli Mohammad <Vempalli.Mohammad@Triconinfotech.com>
2024-01-02 16:47:11 +0530

    intial commit

---
+++ packages/common-ui/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Button from './Components/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button width="100px">Test</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ packages/common-ui/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import Button from './Components/Button';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
+export {
+  Button
+}
