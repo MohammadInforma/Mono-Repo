68a0c343aa4e534560deb84b1de0739068dc6054
Vempalli Mohammad <Vempalli.Mohammad@Triconinfotech.com>
2024-01-03 11:57:54 +0530

    updated code

--- packages/common-ui/src/index.js
+++ packages/common-ui/src/index.js
@@ -4,12 +4,12 @@ import './index.css';
 import App from './App';
 import Button from './Components/Button';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 
 export {
